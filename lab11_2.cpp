187cde80661a6473364daafbc0d7fdacea7c1899
thidayu <tanthidayu_0000@hotmail.com>
2021-12-17 17:11:09 +0700

    "lab11.2 finished"

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,11 +6,20 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------\n";
+ dest << "AR CHI\n";
+ dest << "AR BOW\n";
+ dest << "CHOW CHOW\n";
+ dest << "BA KA\n";
+ dest << "BA GEAR\n";
+ dest << "CHEER CHEER\n";
+ dest << "WE ARE ALL\n";
+ dest << "ENTANEER\n";
+ dest << "CMU\n";
+ dest << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
